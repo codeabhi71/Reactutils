78bb367a273519b860f777c570fe2ec503266931
codeabhi71 <codeabhi71@gmail.com>
2023-02-04 12:48:46 +0530

    Updates

---
+++ precache-manifest.0b11726adce8375bf467d8995e525e1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6ec021bd47bfa15123a",
+    "url": "/Reactutils/static/css/main.3e08d0b5.chunk.css"
+  },
+  {
+    "revision": "d6ec021bd47bfa15123a",
+    "url": "/Reactutils/static/js/main.d6ec021b.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/Reactutils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/Reactutils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "e967b5e681dc7bee05d8",
+    "url": "/Reactutils/static/js/runtime~main.e967b5e6.js"
+  },
+  {
+    "revision": "c2110710b95d7e04c825077db40dadf3",
+    "url": "/Reactutils/index.html"
+  }
+];
